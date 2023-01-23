1e1fcc0b23036c94879816edafe70d9125361c89
Code Vector <117361107+OneCodeVector@users.noreply.github.com>
2023-01-23 15:21:48 +0000

    Final Setup

--- DAX/module.js
+++ DAX/module.js
@@ -1,3 +1,57 @@
-function test(){
-    console.log(1)
+var DAX_CALCULATOR_OBJECT = "body -- dax -- dde & dco"
+var DAX_DISPLAY_ELEMENT = "body -- dax -- dde ~! ddo"
+
+class Calculator{
+    constructor(Element){
+        this.Element = Element
+        this.API = Desmos.GraphingCalculator(Element);
+        this.Line_Data = {}
+        this.Size = 0
+    }
+
+    appendLine(Data){
+        this.Size += 1
+        this.API.setExpression({id: this.Size, latex: Data.Latex})
+        this.Line_Data
+    }
+
+    editLine(Data){
+        if (this.Size <= Data.Line){
+            this.API.setExpression({id: Data.Line, latex: Data.Latex})
+        }
+    }
+
+    clearLine(Data){
+        if (this.Size <= Data.Line){
+            this.API.setExpression({id: Data.Line, latex: ""})
+        }
+    }
+}
+
+class DAX_OBJECT{
+    constructor(Type, Settings){
+        this.Type = Type
+        this.Settings = Settings
+
+        switch (this.Type){
+            case DAX_CALCULATOR_OBJECT:
+                if (Settings.Element == DAX_DISPLAY_ELEMENT){
+                    this.Element = document.getElementById("DAX_DISPLAY_ELEMENT")
+                } else {
+                    //stuff will go here in later update
+                    this.Element = null
+                }
+                break
+                
+            case null:
+                //stuff will go here in a later update
+                this.Element = null
+                
+            this.Size = Settings.Size
+            this.Width = this.Size[0]
+            this.Height = this.Size[1]
+
+            this.Element.style = "width: "+this.Width+"px; height: "+this.Height+"px;"
+        }
+    }
 }
--- test/script.js
+++ test/script.js
@@ -1,3 +1,12 @@
-var elt = document.getElementById('calculator');
-var calculator = Desmos.GraphingCalculator(elt);
-calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
+var DDO = new DAX_OBJECT( // dax display object
+    DAX_CALCULATOR_OBJECT, {
+        Element: DAX_DISPLAY_ELEMENT,
+        Size: [600,400]
+    }    
+)
+
+var Calc = new Calculator(DDO.Element)
+
+Calc.appendLine({Latex: "y=x"})
+Calc.editLine({Line: 1, Latex: "y=x^2"})
+Calc.clearLine({Line: 1})
